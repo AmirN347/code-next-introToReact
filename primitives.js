db17bb351d8168d2344fee74fbc52bf32759cb7d
AmirN347 <108190581+AmirN347@users.noreply.github.com>
2023-04-21 17:36:18 -0400

    Modifieid primitive.js files

--- primitives.js
+++ primitives.js
@@ -2,18 +2,18 @@ import { render } from './util';
 
 // TODO: Declare a variable, assign it a number, and return it.
 render('primitives1', () => {
-  let beverageNumber = 3;
+  const beverageNumber = 3;
   return beverage;
 });
 
 // TODO: Declare a variable, assign it a boolean, and return it.
 render('primitives2', () => {
-  let openForWork = true;
+  const openForWork = true;
   return openForWork;
 });
 
 // TODO: Declare a variable, assign it to null, and return it.
 render('primitives3', () => {
-  let pringles = null;
+  const pringles = null;
   return pringles;
 });
